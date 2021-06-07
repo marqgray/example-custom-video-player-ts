0db64034226679dcfbf26f570b704cd144437638
Marq Gray <marquis.s.sandifer@gmail.com>
2021-06-07 18:08:42 -0500

    Added timestamp update functionality

--- script.ts
+++ script.ts
@@ -34,6 +34,18 @@ const updateProgress = () => {
   htmlElements.progress.value = String(
     (htmlElements.video.currentTime / htmlElements.video.duration) * 100
   );
+
+  let minutes: string = String(Math.floor(htmlElements.video.currentTime / 60));
+  if (Number(minutes) < 10) {
+    minutes = "0" + minutes;
+  }
+
+  let seconds: string = String(Math.floor(htmlElements.video.currentTime % 60));
+  if (Number(seconds) < 10) {
+    seconds = "0" + seconds;
+  }
+
+  htmlElements.timestamp.innerHTML = `${minutes}:${seconds}`;
 };
 
 const stopVideo = () => {
